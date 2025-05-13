3bdf67c7f6b1fac9697ec9e3d68167b939a4563e
KeeganVForsman <keeganforsman@gmail.com>
2025-05-13 10:44:10 +0200

    inputs and shooting

--- BubbleBeatz/Assets/Scripts/PlayerMovement.cs
+++ BubbleBeatz/Assets/Scripts/PlayerMovement.cs
@@ -1,58 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    [SerializeField] private float JumpForce = 12f;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce = 12f;
     [SerializeField] private float fallMultiplier = 2.5f;
     [SerializeField] private float lowJumpMultiplier = 2f;
-    public Rigidbody2D RB;
-    private float nextFireTime = 0f;
     [SerializeField] private GameObject notePrefab;
     [SerializeField] private Transform firePoint;
     [SerializeField] private float noteCooldown = 0.3f;
+
+    private Rigidbody2D rb;
+    private PlayerInputActions inputActions;
+    private Vector2 moveInput;
+    private bool jumpPressed;
+    private bool shootPressed;
+    private float nextFireTime = 0f;
+
+
     private void Awake()
     {
-        RB = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
 
+        inputActions = new PlayerInputActions();
+        inputActions.Enable();
+
+        inputActions.Gameplay.Jump.performed += ctx => jumpPressed = true;
+        inputActions.Gameplay.Shoot.performed += ctx => shootPressed = true;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //Horizontal Movement
-        RB.velocity = new Vector2(horizontalInput*speed, RB.velocity.y);
-        if (horizontalInput>0)
-        {
+        moveInput = inputActions.Gameplay.Right.ReadValue<float>() > 0
+            ? Vector2.right
+            : inputActions.Gameplay.Left.ReadValue<float>() > 0
+                ? Vector2.left
+                : Vector2.zero;
+
+        // Flip sprite
+        if (moveInput.x > 0)
             transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (horizontalInput <0 )
-        {
+        else if (moveInput.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
-        }
-        //Jumping
-        if (Input.GetKey(KeyCode.Space) && Mathf.Abs(RB.velocity.y)<0.01f)
+
+        // Jump
+        if (jumpPressed && Mathf.Abs(rb.velocity.y) < 0.01f)
         {
-            RB.velocity = new Vector2(RB.velocity.x, JumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpPressed = false;
         }
-        //gravity
-        if (RB.velocity.y <0)
+
+        // Gravity adjustments
+        if (rb.velocity.y < 0)
         {
-            RB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if(RB.velocity.y >0 && !Input.GetKey(KeyCode.Space))
+        else if (rb.velocity.y > 0 && !inputActions.Gameplay.Jump.ReadValue<float>().Equals(1f))
         {
-            RB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.F)&& Time.deltaTime>=nextFireTime)
+
+        // Shoot
+        if (shootPressed && Time.time >= nextFireTime)
         {
             ShootNote();
-            nextFireTime = Time.deltaTime + noteCooldown; 
+            nextFireTime = Time.time + noteCooldown;
+            shootPressed = false;
         }
-        
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+    }
+
     private void ShootNote()
     {
         GameObject note = Instantiate(notePrefab, firePoint.position, Quaternion.identity);
