379757d116603d73fb296b6aaa6a98240d80b691
KeeganVForsman <keeganforsman@gmail.com>
2025-05-13 11:23:19 +0200

    physics, input buffer and coyote time

--- BubbleBeatz/Assets/Scripts/PlayerMovement.cs
+++ BubbleBeatz/Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,41 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce = 12f;
     [SerializeField] private float fallMultiplier = 2.5f;
     [SerializeField] private float lowJumpMultiplier = 2f;
+
+    [Header("Shooting")]
     [SerializeField] private GameObject notePrefab;
     [SerializeField] private Transform firePoint;
     [SerializeField] private float noteCooldown = 0.3f;
+    [SerializeField] private float shootBufferTime = 0.2f;
+
+    [Header("Jump Buffer & Coyote Time")]
+    [SerializeField] private float jumpBufferTime = 0.1f;
+    [SerializeField] private float coyoteTime = 0.1f;
 
     private Rigidbody2D rb;
     private PlayerInputActions inputActions;
     private Vector2 moveInput;
-    private bool jumpPressed;
-    private bool shootPressed;
-    private float nextFireTime = 0f;
 
+    private float jumpBufferCounter;
+    private float coyoteTimeCounter;
+    private float shootBufferCounter;
+    private float nextFireTime;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
         inputActions = new PlayerInputActions();
         inputActions.Enable();
 
-        inputActions.Gameplay.Jump.performed += ctx => jumpPressed = true;
-        inputActions.Gameplay.Shoot.performed += ctx => shootPressed = true;
+        inputActions.Gameplay.Jump.performed += ctx => jumpBufferCounter = jumpBufferTime;
+        inputActions.Gameplay.Shoot.performed += ctx => shootBufferCounter = shootBufferTime;
     }
 
     private void OnDisable()
@@ -38,6 +45,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // Movement input
         moveInput = inputActions.Gameplay.Right.ReadValue<float>() > 0
             ? Vector2.right
             : inputActions.Gameplay.Left.ReadValue<float>() > 0
@@ -50,11 +58,27 @@ public class PlayerMovement : MonoBehaviour
         else if (moveInput.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
+        // Timers
+        jumpBufferCounter -= Time.deltaTime;
+        shootBufferCounter -= Time.deltaTime;
+
+        bool isGrounded = Mathf.Abs(rb.velocity.y) < 0.01f;
+
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
         // Jump
-        if (jumpPressed && Mathf.Abs(rb.velocity.y) < 0.01f)
+        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            jumpPressed = false;
+            jumpBufferCounter = 0f;
+            coyoteTimeCounter = 0f;
         }
 
         // Gravity adjustments
@@ -67,12 +91,12 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
 
-        // Shoot
-        if (shootPressed && Time.time >= nextFireTime)
+        // Shoot with buffer
+        if (shootBufferCounter > 0 && Time.time >= nextFireTime)
         {
             ShootNote();
             nextFireTime = Time.time + noteCooldown;
-            shootPressed = false;
+            shootBufferCounter = 0f;
         }
     }
 
